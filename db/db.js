66bf83663139924d0f1e93901ceb40aac8d36124
Swapnil Markhedkar <Swapnil.sam.13@gmail.com>
2019-02-26 12:53:39 +0530

    Added CRUD Operations

---
+++ db/db.js
@@ -0,0 +1,40 @@
+const {MongoClient} = require('mongodb');
+const {ObjectID} = require('mongodb');//es6 Destructuring
+const dbName = "groundwater";
+const url = 'mongodb+srv://hyperion101010:hyperion101010@cluster0-2y3cu.mongodb.net';
+const mongoOptions = {
+	useNewUrlParser : true
+};
+
+const state = {
+	db : null
+};
+
+const connect = (cb)=>{
+	if(state.db)
+		cb();
+	else{
+		 MongoClient.connect(url,mongoOptions,(err,client)=>{
+		 	if(err)
+		 		cb(err);
+		 	else{
+		 		state.db = client.db(dbName);
+		 		cb();
+		 		console.log('Connected to DatabaseCloud');
+		 	}
+		});
+	}	
+}
+
+const getPrimaryKey = (_id)=>{
+	return ObjectID(_id);
+}
+
+//returns client.db(dbName)
+const getDB = ()=>{
+	return state.db;
+}
+
+module.exports = {getDB, connect, getPrimaryKey};
+
+
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express');
 
 const app = express();
 
+const db = require('./db/db.js');
+const collection = "water";
+
 app.use('/', express.static(__dirname + '/dist/Hackathon'));
 app.set('views', __dirname + '/dist/Hackathon'); // Set views (index.html) to root directory
 app.engine('html', ejs.renderFile); // Default for express is Jade as the rendering engine. Change that to EJS for HTML over JADE
@@ -22,6 +25,79 @@ app.get('/login', (req, res) => {
     res.end();
 });
 
-http.createServer(app).listen(port, () => {
-    console.log('Express server listening on port: ' + port);
+//CRUD Operations
+//POST Data
+app.post('/postData', (req,res)=>{
+	const userInput = req.body;
+	db.getDB().collection(collection)
+	.insertOne(userInput,(err,result)=>{
+		if(err)
+			console.log(err)
+
+		else{
+				res.json({
+					result,
+					document : result.ops[0]
+				});
+		}	
+	});
 });
+
+//GET Data
+app.get('/getData',(req,res)=>{
+	db.getDB().collection(collection).find({})
+		.toArray((err,docs)=>{
+			if(err)
+				console.log(err);
+			else
+				res.json(docs);
+			
+		});
+});
+
+//UPDATE Data
+app.put('/:id', (req,res)=>{
+	const ID = req.params.id;
+	const userInput = req.body;
+
+	db.getDB().collection(collection).findOneAndUpdate(
+		{_id : db.getPrimaryKey(ID)},
+		{$set : {cost_for_water_usage : userInput.cost_for_water_usage}},
+		(err,result)=>{
+			if(err)
+				console.log(err);
+
+			else
+				res.json(result);
+		}
+	);
+});
+
+//DELETE Data
+app.delete('/:id', (req,res)=>{
+	const ID = req.params.id;
+
+	db.getDB().collection(collection).findOneAndDelete(
+		{_id : db.getPrimaryKey(ID)},
+		(err,result)=>{
+			if(err)
+				console.log(err);
+
+			else
+				res.json(result);
+		}
+	);
+});
+
+//Establish Connection
+db.connect((err)=>{
+	if(err)
+		console.log('Unable to Connect to Database');
+
+	else{
+		app.listen(port, ()=>{
+			console.log(`Server up and Running on ${port}`);
+		});
+	}
+});
+
