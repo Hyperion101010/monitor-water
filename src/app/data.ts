42294345006989a2446c6e19017807b1547684b1
Devendra Phadke <devendraphadke@Devendras-MacBook-Air.local>
2019-03-03 15:45:15 +0530

    Added sample data for prediction page

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,17 +1,17 @@
-  export var single = [
-    {
-      "name": "Week1",
-      "value": 89400
-    },
-    {
-      "name": "Week2",
-      "value": 50000
-    },
-    {
-      "name": "Week3",
-      "value": 72000
-    }
-  ];
+export var single = [
+  {
+    "name": "Week1",
+    "value": 89400
+  },
+  {
+    "name": "Week2",
+    "value": 50000
+  },
+  {
+    "name": "Week3",
+    "value": 72000
+  }
+];
 
 /*export var multi = [
   {
@@ -65,92 +65,92 @@ export var single1 = [
   }
 ];
 
-  export var water_usage = [
-    {
-      "name": "Water used",
-      "value": 100
-    },
-    {
+export var water_usage = [
+  {
+    "name": "Water used",
+    "value": 100
+  },
+  {
     "name": "Water available",
     "value": 203
-    }
-  ];
+  }
+];
 
-  export var multi_usage = [
-    {
-      "name": "Water used",
-      "series": [
-        {
-          "year" : 2010,
-          "usage" : 4545
-        },
-        {
-          "year" :2011,
-          "usage" :1545
-        }
-      ]
-    },
-    {
-      "name": "Water available",
-      "series": [
-        {
-          "year": 2010,
-          "usage": 2545
-        },
-        {
-          "year": 2011,
-          "usage": 4545
-        }
-      ]
-    }
-  ];
+export var multi_usage = [
+  {
+    "name": "Water used",
+    "series": [
+      {
+        "year": 2010,
+        "usage": 4545
+      },
+      {
+        "year": 2011,
+        "usage": 1545
+      }
+    ]
+  },
+  {
+    "name": "Water available",
+    "series": [
+      {
+        "year": 2010,
+        "usage": 2545
+      },
+      {
+        "year": 2011,
+        "usage": 4545
+      }
+    ]
+  }
+];
 
-  export var month = [
-    {
-    "name" : "January",
-    "value" : 10
-    },
-    {
-      "name" : "February",
-      "value" : 20
-    },
-    {
-      "name" : "March",
-      "value" :34
-    },
-    {
-      "name" : "April",
-      "value" : 56,
-    },
-    {
-    "name" : "May",
-    "value" : 34
-    },
-    {
-      "name" : "June",
-      "value" :23
-    },
-    {
-      "name" : "July",
-      "value" : 23
-    },
-    {
-      "name" : "August",
-      "value" : 22
-    },
-    {
-      "name": "Spetember",
-      "value" : 11
-    },
-    {
-      "name" : "November",
-      "value" : 22
-    },
-    {
-      "name" : "December",
-      "value" : 22
-    }
-  ]
+export var month = [
+  {
+    "name": "January",
+    "value": 10
+  },
+  {
+    "name": "February",
+    "value": 20
+  },
+  {
+    "name": "March",
+    "value": 34
+  },
+  {
+    "name": "April",
+    "value": 56,
+  },
+  {
+    "name": "May",
+    "value": 34
+  },
+  {
+    "name": "June",
+    "value": 23
+  },
+  {
+    "name": "July",
+    "value": 23
+  },
+  {
+    "name": "August",
+    "value": 22
+  },
+  {
+    "name": "Spetember",
+    "value": 11
+  },
+  {
+    "name": "November",
+    "value": 22
+  },
+  {
+    "name": "December",
+    "value": 22
+  }
+]
 
 export var month2 = [
   {
@@ -185,7 +185,7 @@ export var month2 = [
     "name": "August",
     "value": 22
   },
-    {
+  {
     "name": "Spetember",
     "value": 11
   },
@@ -200,99 +200,139 @@ export var month2 = [
   }
 ]
 
-  export var multi = [
-    {
-      "name": "Week1",
-      "series": [
-        {
-          "name": "Monday",
-          "value": 3700
-        },
-        {
-          "name": "Tuesday",
-          "value": 3500
-        },
-        {
-          "name": "Wednesday",
-          "value": 4800
-        },
-        {
-          "name": "Thursday",
-          "value": 2100
-        },
-        {
-          "name": "Friday",
-          "value": 3200
-        },{
-          "name": "Saturday",
-          "value": 4500
-        },{
-          "name": "Sunday",
-          "value": 5200
-        },
-      ]
-    },
+export var multi = [
+  {
+    "name": "Week1",
+    "series": [
+      {
+        "name": "Monday",
+        "value": 3700
+      },
+      {
+        "name": "Tuesday",
+        "value": 3500
+      },
+      {
+        "name": "Wednesday",
+        "value": 4800
+      },
+      {
+        "name": "Thursday",
+        "value": 2100
+      },
+      {
+        "name": "Friday",
+        "value": 3200
+      }, {
+        "name": "Saturday",
+        "value": 4500
+      }, {
+        "name": "Sunday",
+        "value": 5200
+      },
+    ]
+  },
 
-    {
-      "name": "Week2",
-      "series": [
-       {
-          "name": "Monday",
-          "value": 5300
-        },
-        {
-          "name": "Tuesday",
-          "value": 3800
-        },
-        {
-          "name": "Wednesday",
-          "value": 2300
-        },
-        {
-          "name": "Thursday",
-          "value": 4200
-        },
-        {
-          "name": "Friday",
-          "value": 2300
-        },{
-          "name": "Saturday",
-          "value": 5600
-        },{
-          "name": "Sunday",
-          "value": 6200
-        },
-      ]
-    },
-  ];
+  {
+    "name": "Week2",
+    "series": [
+      {
+        "name": "Monday",
+        "value": 5300
+      },
+      {
+        "name": "Tuesday",
+        "value": 3800
+      },
+      {
+        "name": "Wednesday",
+        "value": 2300
+      },
+      {
+        "name": "Thursday",
+        "value": 4200
+      },
+      {
+        "name": "Friday",
+        "value": 2300
+      }, {
+        "name": "Saturday",
+        "value": 5600
+      }, {
+        "name": "Sunday",
+        "value": 6200
+      },
+    ]
+  },
+];
 
-  export var reuse = [
-    {
-      "name": "Maharashtra",
-      "value": 60
-    },
-    {
-      "name": "Tripura",
-      "value": 41.4162806088683
-    },
-    {
-      "name": "Lakshadweep (UT)",
-      "value": 40.57553956834533
-    },
-    {
-      "name": "Daman and Diu (UT)",
-      "value": 37.12
-    },
-    {
-      "name": "Mizoram",
-      "value": 40.374581939799334
-    },
-    {
-      "name": "Kerala",
-      "value": 48.24324324324324
-    }
-  ];
+export var reuse = [
+  {
+    "name": "Maharashtra",
+    "value": 60
+  },
+  {
+    "name": "Tripura",
+    "value": 41.4162806088683
+  },
+  {
+    "name": "Lakshadweep (UT)",
+    "value": 40.57553956834533
+  },
+  {
+    "name": "Daman and Diu (UT)",
+    "value": 37.12
+  },
+  {
+    "name": "Mizoram",
+    "value": 40.374581939799334
+  },
+  {
+    "name": "Kerala",
+    "value": 48.24324324324324
+  }
+];
 
+export var waterExtraction = [{ "name": 2020, "value": 20.596462264150944 },
+{ "name": 2021, "value": 20.568867924528305 }, { "name": 2022, "value": 20.54127358490566 },
+{ "name": 2023, "value": 20.513679245283022 }, { "name": 2024, "value": 20.486084905660377 },
+{ "name": 2025, "value": 20.45849056603774 }, { "name": 2026, "value": 20.430896226415094 },
+{ "name": 2027, "value": 20.403301886792455 }, { "name": 2028, "value": 20.37570754716981 },
+{ "name": 2029, "value": 20.348113207547172 }];
+
+
+export var groudwaterLevel = [
+  {
+    "name": "Week1",
+    "series": [
+      {"name":2020,"value":6.6136792452830235},
+      {"name":2021,"value":6.672877358490567},
+      {"name":2022,"value":6.732075471698124},
+      {"name":2023,"value":6.791273584905667},
+      {"name":2024,"value":6.8504716981132106},
+      {"name":2025,"value":6.909669811320768},
+      {"name":2026,"value":6.968867924528311},
+      {"name":2027,"value":7.028066037735854},
+      {"name":2028,"value":7.087264150943398},
+      {"name":2029,"value":7.146462264150955}
+    ]
+  },
+  {
+    "name": "Week2",
+    "series": [
+      {"name":2020,"value":6.6136792452830235},
+      {"name":2021,"value":6.672877358490567},
+      {"name":2022,"value":6.732075471698124},
+      {"name":2023,"value":6.791273584905667},
+      {"name":2024,"value":6.8504716981132106},
+      {"name":2025,"value":6.909669811320768},
+      {"name":2026,"value":6.968867924528311},
+      {"name":2027,"value":7.028066037735854},
+      {"name":2028,"value":7.087264150943398},
+      {"name":2029,"value":7.146462264150955}
+    ]
+  }
+];
 /*
 export var ref = [
   {
--- src/app/prediction/prediction.component.ts
+++ src/app/prediction/prediction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { waterExtraction, groudwaterLevel } from '../data';
 
 @Component({
   selector: 'app-prediction',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PredictionComponent implements OnInit {
 
-  constructor() { }
+  waterExtraction: any[];
+  groudwaterLevel: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  showYAxisLabel = true;
+  xAxisLabel = 'Year';
+  yAxisLabel = 'Water Extracted (meter cube)';
+  xAxisLabel1 = 'Day';
+  yAxisLabel2 = 'Ground water level (mbgl)';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { waterExtraction, groudwaterLevel });
+  }
 
   ngOnInit() {
+  }
+
+  onSelect(event) {
+    console.log(event);
   }
 
 }
